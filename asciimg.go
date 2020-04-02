ee0cfe8b404527fe3ecc107c651b2258b51759ac
kineticmarginal <43065450+kineticmarginal@users.noreply.github.com>
2020-04-02 22:37:51 +0300

    solved

--- asciimg.go
+++ asciimg.go
@@ -6,18 +6,41 @@ import (
 	"image"
 	"image/color"
 	_ "image/jpeg"
+	"io/ioutil"
 	// Side-effect import.
 	// Сайд-эффект — добавление декодера PNG в пакет image.
 	_ "image/png"
 	"os"
 	// Внешняя зависимость.
 	"golang.org/x/image/draw"
+	"github.com/buger/goterm"
+	colorkit "github.com/gookit/color"
 )
 
-func scale(img image.Image, w int, h int) image.Image {
+var (
+	out   = flag.String("o", "", "file to write")
+	noscale = flag.Bool("noscale", false, "Do not scale the image")
+	width = flag.Int("w", 200, "Image width")
+	height = flag.Int("h", 40, "Image height")
+	termimal_size = flag.Bool("term", false, "Get termial size")
+	colorize = flag.Bool("c", false, "Colorize image")
+)
+
+func resize(img image.Image, w int, h int) image.Image {
 	dstImg := image.NewRGBA(image.Rect(0, 0, w, h))
 	draw.NearestNeighbor.Scale(dstImg, dstImg.Bounds(), img, img.Bounds(), draw.Over, nil)
 	return dstImg
+}
+
+func scale_image(img image.Image) image.Image {
+	if *termimal_size {
+		*width = goterm.Width()
+		*height = goterm.Height()
+	}
+	if *out == "" && !*noscale {
+		img = resize(img, *width, *height)
+	}
+	return img
 }
 
 func decodeImageFile(imgName string) (image.Image, error) {
@@ -31,19 +54,19 @@ func decodeImageFile(imgName string) (image.Image, error) {
 	return img, err
 }
 
-func processPixel(c color.Color) rune {
+func processPixel(c color.Color) byte {
+	symbols := []rune("MND8OZ$7I?+=~:,.  ")
 	gc := color.GrayModel.Convert(c)
 	r, _, _, _ := gc.RGBA()
 	r = r >> 8
-
-	// symbols := []rune("@80GCLft1i;:,. ")
-	return '0'
+	pos := uint32(r * 16 / 255)
+	return byte(symbols[pos])
 }
 
-func convertToAscii(img image.Image) [][]rune {
-	textImg := make([][]rune, img.Bounds().Dy())
+func convertToAscii(img image.Image) [][]byte {
+	textImg := make([][]byte, img.Bounds().Dy())
 	for i := range textImg {
-		textImg[i] = make([]rune, img.Bounds().Dx())
+		textImg[i] = make([]byte, img.Bounds().Dx())
 	}
 
 	for i := range textImg {
@@ -69,11 +92,43 @@ func main() {
 		os.Exit(1)
 	}
 
+	img = scale_image(img)
+
 	textImg := convertToAscii(img)
-	for i := range textImg {
-		for j := range textImg[i] {
-			fmt.Printf("%c", textImg[i][j])
+	
+	if *out != "" {
+		f, err := os.Create(*out)
+		if err != nil {
+			print(err)
+			os.Exit(1)
 		}
-		fmt.Println()
+
+		for i := range textImg {
+			err = ioutil.WriteFile(*out, append([]byte(textImg[i]), []byte("\n")...), 0644)
+			if err != nil {
+				fmt.Println("Error:", err.Error())
+				os.Exit(1)
+			}
+		}
+		defer f.Close()
+		fmt.Println("Image in your file!")
+	} else {
+		if *colorize {
+			for i := range textImg {
+				for j := range textImg[i] {
+					r, g, b, _ := color.GrayModel.Convert(img.At(j, i)).RGBA()
+					c := colorkit.RGB(uint8(r), uint8(g), uint8(b))
+					c.Print(string(textImg[i][j]))
+				}
+				fmt.Println()
+			}
+		} else {
+			for i := range textImg {
+				for j := range textImg[i] {
+					fmt.Print(string(textImg[i][j]))
+				}
+				fmt.Println()
+			}
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module asciimg
 
 go 1.12
 
-require golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+require (
+	github.com/buger/goterm v0.0.0-20200322175922-2f3e71b85129
+	github.com/gookit/color v1.2.3
+	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
